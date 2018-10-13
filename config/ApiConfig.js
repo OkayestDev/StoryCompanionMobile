98ace1c4404fcd41d386a893939d270380d6cecd
Kyle Richardson <kyle@minute7.com>
2018-10-13 09:26:16 -0400

    Fixing ApiConfig

--- config/ApiConfig.js
+++ config/ApiConfig.js
@@ -1,7 +1,7 @@
 export const API_CONFIG = {
     // Local
-    url: 'http://10.0.2.1:8000', //Symphony Server
+    // url: 'http://10.0.2.1:8000', //Symphony Server
     // Production
-    // url: 'http://api.story-companion.com',
+    url: 'http://api.story-companion.com',
     url_suffix: '/',
 }
